aab5d5cb9c13d45f8cf856618921ed236e43ac8b
SophWed1 <111957156+SophWed1@users.noreply.github.com>
2023-01-20 15:26:03 +1100

    spooky

---
+++ src/main/java/frc/robot/commands/FollowTarget.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+public class FollowTarget extends CommandBase {
+  private final Drivetrain m_Drivetrain;
+  private final DoubleSupplier m_Offset;
+  /** Creates a new FollowTarget. */
+  public FollowTarget(Drivetrain drivetrain, DoubleSupplier getOffset) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_Drivetrain = drivetrain;
+    m_Offset = getOffset;
+    addRequirements(m_Drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double lols = m_Offset.getAsDouble();
+    m_Drivetrain.drive(0, 0, 0.3*lols, false);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
