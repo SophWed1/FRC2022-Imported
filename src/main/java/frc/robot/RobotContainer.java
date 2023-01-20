963d877398cd8174aba27241c3ae92e5fd68536e
DolalaBanana <84213320+mulanthebaguette@users.noreply.github.com>
2023-01-20 13:55:26 +1100

    effdjsf

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,131 @@
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.cameraserver.CameraServer;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import frc.robot.commands.ClimbReverse;
+import frc.robot.commands.ClimbRobot;
+import frc.robot.commands.ClimberIn;
+import frc.robot.commands.ClimberOut;
+import frc.robot.commands.CloseHook;
+import frc.robot.commands.FeedBall;
+import frc.robot.commands.FeedBallForShooter;
+import frc.robot.commands.IntakeBall;
+import frc.robot.commands.IntakeIn;
+import frc.robot.commands.IntakeOut;
+import frc.robot.commands.OpenHook;
+import frc.robot.commands.ReverseBall;
+import frc.robot.commands.ReverseClimb;
+import frc.robot.commands.ReverseIntake;
+import frc.robot.commands.ReverseShoot;
+import frc.robot.commands.SenseClimber;
+import frc.robot.commands.ShootBall;
+import frc.robot.commands.Group.Group.Feeder.IndexFirstBall;
+import frc.robot.commands.Group.Group.Intake.FullIntake;
+import frc.robot.commands.Group.Group.Intake.IntakeMaster;
+import frc.robot.commands.Group.Group.Shooter.ShootBalls;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter.Shooter;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  XboxController driverController = new XboxController(0);
+  XboxController operatorController = new XboxController(1); 
+
+  // The robot's subsystems and commands are defined here...
+  private final Drivetrain drivetrain = new Drivetrain();
+  private final Feeder m_feeder = new Feeder();
+  private final Shooter m_shooter = new Shooter();
+  private final Intake m_intake = new Intake();
+  private final Climber m_climber = new Climber();
+ 
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    
+    drivetrain.setDefaultCommand(
+      new RunCommand(
+        () -> drivetrain.drive( //removed negative
+          Drivetrain.deadZone(-3.6*driverController.getLeftY()),
+          Drivetrain.deadZone(-3.6*driverController.getLeftX()),
+          Drivetrain.deadZone(-3.6*driverController.getRightX()),
+          true), 
+        drivetrain)
+    );
+
+    configureButtonBindings();
+
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+    final JoystickButton Abutton = new JoystickButton(operatorController,1);
+    final JoystickButton Bbutton = new JoystickButton(operatorController,2);
+    final JoystickButton Xbutton = new JoystickButton(operatorController,3);
+    final JoystickButton Ybutton = new JoystickButton(operatorController,4);
+    final JoystickButton LeftBumper = new JoystickButton(operatorController,5);
+    final JoystickButton RightBumper = new JoystickButton(operatorController,6);  
+    final JoystickButton leftStartButton = new JoystickButton(operatorController,7);
+    final JoystickButton leftButton = new JoystickButton(operatorController,9);
+    final JoystickButton rightButton = new JoystickButton(operatorController,10);
+
+    Abutton.whileHeld(new IntakeMaster(m_intake,m_feeder));
+    //Ybutton.whileHeld(new ReverseShoot(m_shooter));
+    Bbutton.whileHeld(new ShootBall(m_shooter));
+    RightBumper.whileHeld(new FeedBallForShooter(m_feeder));
+    Ybutton.whileHeld(new SenseClimber(m_climber));
+    //LeftBumper.whileHeld(new IntakeBall(m_intake));
+
+    
+    final JoystickButton buttonA = new JoystickButton(driverController,1);
+    final JoystickButton buttonB = new JoystickButton(driverController,2);
+    final JoystickButton buttonX = new JoystickButton(driverController,3);
+    final JoystickButton buttonY = new JoystickButton(driverController,4);
+    final JoystickButton BumperLeft = new JoystickButton(driverController,5);
+    final JoystickButton BumperRight = new JoystickButton(driverController,6);
+    final JoystickButton leftStartButtonDriver = new JoystickButton(driverController,7);
+    
+    buttonY.whenPressed(() -> drivetrain.resetGyro(),drivetrain); 
+    buttonB.whileHeld(new ClimberOut(m_climber));
+    //buttonX.whenPressed(new ClimberIn(m_climber));
+    BumperLeft.whileHeld(new OpenHook(m_climber));
+    //BumperRight.whileHeld(new CloseHook(m_climber));
+    buttonA.whileHeld(new ClimbRobot(m_climber));
+    leftStartButtonDriver.whileHeld(new ReverseClimb(m_climber));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+
+}
---
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainConstants.java
@@ -0,0 +1,59 @@
+package frc.robot.subsystems.drivetrain;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class DrivetrainConstants {
+    public static final int 
+    LF_DRIVE_MOTOR_PORT = 11,
+    LB_DRIVE_MOTOR_PORT = 12,
+    RF_DRIVE_MOTOR_PORT = 13,
+    RB_DRIVE_MOTOR_PORT = 10;
+
+public static final int
+    LF_STEER_MOTOR_PORT = 15,
+    LB_STEER_MOTOR_PORT = 16, 
+    RF_STEER_MOTOR_PORT = 17,
+    RB_STEER_MOTOR_PORT = 14; 
+
+public static final int
+    LF_ANGLE_ENCODER_PORT = 19,
+    LB_ANGLE_ENCODER_PORT = 20,
+    RF_ANGLE_ENCODER_PORT = 21,
+    RB_ANGLE_ENCODER_PORT = 18;
+
+public static final double 
+    //x is forwards y is right )
+    X_LENGTH_BETWEEN_MODULES = 1,
+    Y_WIDTH_BETWEEN_MODULES = 1,
+    MODULE_DISTANCE_FROM_CENTRE_LENGTH = X_LENGTH_BETWEEN_MODULES / 2,
+    MODULE_DISTANCE_FROM_CENTRE_WIDTH = Y_WIDTH_BETWEEN_MODULES / 2;
+
+public static final Translation2d[] moduleOffset = {
+    // positive x values represent moving towards front of robot 
+    // positive y values represent moving towards left of robot
+    new Translation2d(MODULE_DISTANCE_FROM_CENTRE_LENGTH,MODULE_DISTANCE_FROM_CENTRE_WIDTH), // front left
+    new Translation2d(-MODULE_DISTANCE_FROM_CENTRE_LENGTH, MODULE_DISTANCE_FROM_CENTRE_WIDTH), //back left
+    new Translation2d(MODULE_DISTANCE_FROM_CENTRE_LENGTH,-MODULE_DISTANCE_FROM_CENTRE_WIDTH), // front right
+    new Translation2d(-MODULE_DISTANCE_FROM_CENTRE_LENGTH,-MODULE_DISTANCE_FROM_CENTRE_WIDTH) //back right
+};
+
+    public static final double angleGearRatio = 12.8;
+
+    public static final double 
+        MAX_SPEED_METRES_PER_SECOND = 4, //0.8
+        GEAR_RATIO = 8.14, //one rotation of wheel from 8.14 shaft rotations
+        CANCODER_RAW_TICKS_PER_180 = 2048,
+        TEN = 10,
+        WHEEL_RADIUS = 0.0508,
+        HALF_ROTATION = CANCODER_RAW_TICKS_PER_180 / 180.0,
+        TALONFX_RESOLUTION = 2048,
+        TICKS_PER_WHEEL_ROTATION = TALONFX_RESOLUTION * GEAR_RATIO,
+        OMEGA = TEN*((2*Math.PI)/TICKS_PER_WHEEL_ROTATION), //conversion from radians per 100ms to 1000ms (1s)
+        VELOCITY_MULTIPLIER = OMEGA * WHEEL_RADIUS;
+        
+}
+
+
+
+
+
